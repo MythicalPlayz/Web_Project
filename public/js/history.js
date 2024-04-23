1463fcb3a3a6b5e24cf98d499fd773aefd9027d4
MythicalPlayz <eyaddodo05@gmail.com>
2024-04-23 02:23:09 +0200

    home - user

--- public/js/history.js
+++ public/js/history.js
@@ -13,7 +13,7 @@ function history(){
     const jobs = getHistory(name);
     const maxN = jobs.length;
     for (let idx = 0; idx < maxN; idx++){
-        let job = jobs[idx];
+        let job = jobs[jobs.length - 1 - idx];
         let e = createElement('div',document.getElementById('jobs'),'job');
         let i = createElement('div',e,'info');
         createElement('h3',i,'jobid',job.jobid);
@@ -22,8 +22,22 @@ function history(){
         createElement('p',e,'status','Status: ' + job.status.substr(0,1).toUpperCase() + job.status.substr(1));
     }
 }
-if (window.location.href.includes('home')){
 
+function historyHome(){
+    const name = JSON.parse(localStorage.getItem('local-account')).username;
+    const jobs = getHistory(name);
+    const maxN = Math.min(jobs.length,3);
+    for (let idx = 0; idx < maxN; idx++){
+        let job = jobs[jobs.length - 1 - idx];
+        let e = createElement('div',document.querySelector('.quick-short'),'job');
+        createElement('h4',e,'jobname',job.jobname);
+        createElement('p',e,'companyname',job.company);
+        createElement('span',e,'status','Status: ' + job.status.substr(0,1).toUpperCase() + job.status.substr(1));
+    }
+}
+
+if (window.location.href.includes('home')){
+    historyHome();
 }
 else{
     history();
