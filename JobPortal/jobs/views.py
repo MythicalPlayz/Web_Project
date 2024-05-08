3d45257df90777970d7603e4044db5344452f23b
MythicalPlayz <eyaddodo05@gmail.com>
2024-05-08 19:49:35 +0300

    jobs implement

--- JobPortal/jobs/views.py
+++ JobPortal/jobs/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def members(request):
-    return HttpResponse("Hello world!")
+def jobs(request):
+    template = loader.get_template('jobs.html')
+    return HttpResponse(template.render())
 
 # Create your views here.
