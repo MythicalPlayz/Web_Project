ff4d0b816187699d2e574a8b3b1c79146bb72080
MythicalPlayz <eyaddodo05@gmail.com>
2024-05-14 03:52:50 +0300

    More Jobs

--- JobPortal/jobs/urls.py
+++ JobPortal/jobs/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('jobs/', views.jobs, name='jobs'),
     path('jobs/details/<str:id>/', views.details, name='details'),
+    path('jobs/add', views.add, name='add'),
+    path('jobs/details/<str:id>/edit', views.edit, name='edit'),
+    path('jobs/details/<str:id>/delete', views.delete, name='delete'),
+
+    #TODO: Add Success/Fail
 ]
--- JobPortal/jobs/views.py
+++ JobPortal/jobs/views.py
@@ -22,4 +22,40 @@ def details(request, id):
     }
     return HttpResponse(template.render(prameters, request))
 
-# Create your views here.
+def add(request):
+    if request.method == 'GET':
+        template = loader.get_template('add_job.html')
+        return HttpResponse(template.render())
+    elif request.method == 'POST':
+        pass
+    else:
+        return HttpResponse("UNSUPPORTED METHOD")
+    # POST REQUEST
+
+def edit(request, id):
+    if request.method == 'GET':
+        try:
+            jobobject = Job.objects.get(id=id)
+        except Job.DoesNotExist:
+            raise Http404("Job does not exist")
+        
+        template = loader.get_template('edit_job.html')
+        prameters = {
+            'job': jobobject,
+        }
+        return HttpResponse(template.render(prameters, request))
+    elif request.method == 'POST':
+        pass
+    else:
+        return HttpResponse("UNSUPPORTED METHOD")
+    # POST REQUEST
+
+def delete(request, id):
+    if request.method == 'DELETE':
+        try:
+            jobobject = Job.objects.get(id=id)
+        except Job.DoesNotExist:
+            raise Http404("Job does not exist")
+        # DELETE REQUEST
+    else:
+        return HttpResponse("UNSUPPORTED METHOD")
