550537920149f5889d0e1ecf4ebbac52d09cd503
MythicalPlayz <eyaddodo05@gmail.com>
2024-06-16 00:47:22 +0300

    Add 404 Page

--- JobPortal/JobPortal/settings.py
+++ JobPortal/JobPortal/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-71&d6f^0tl&ly4siaa!e0e^@%uevk3!6)w1100%8^07+40v-2-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False #True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'JobPortal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
